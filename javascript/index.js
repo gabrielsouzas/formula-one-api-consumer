a8f324d91d470f83ff13ab9513b5549ed4d71f7c
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2023-08-30 16:54:00 -0300

    Estilização página principal

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,15 @@
-import { fetchRaceSchedule } from "../services/services.js";
+import { fetchDriversStandings, fetchRaceSchedule } from "../services/services.js";
 
 const tableBody = document.getElementById('table-body');
+const menuButtons = document.querySelectorAll('.menu button');
 
-const fillTable = async () => {
+var clickedButtonId = 'mn-rc-schd';
+
+const fillTableRacesSchedule = async (year = 'current') => {
     try {
-        const races = await fetchRaceSchedule();
+        const races = await fetchRaceSchedule(year);
+
+        tableBody.innerHTML = '';
 
         races.forEach(races => {
             const row = document.createElement('tr');
@@ -41,4 +46,116 @@ const fillTable = async () => {
     }
 }
 
-fillTable();
+const fillTableDriversStandings = async () => {
+    try {
+        const races = await fetchDriversStandings();
+
+        tableBody.innerHTML = '';
+
+        races.forEach(races => {
+            const row = document.createElement('tr');
+
+            const cellSeason = document.createElement('td');
+            const cellRound = document.createElement('td');
+            const cellRaceName = document.createElement('td');
+            const cellDate = document.createElement('td');
+            const cellTime = document.createElement('td');
+            const cellSprint = document.createElement('td');
+            const cellCircuit = document.createElement('td');
+
+            cellSeason.textContent = races.season;
+            cellRound.textContent = races.round;
+            cellRaceName.textContent = races.raceName;
+            cellDate.textContent = races.date;
+            cellTime.textContent = races.time;
+            cellSprint.textContent = 'Sprint' in races ? races.Sprint.date : '';
+            cellCircuit.textContent = races.Circuit.circuitName;
+
+            row.appendChild(cellSeason);
+            row.appendChild(cellRound);
+            row.appendChild(cellRaceName);
+            row.appendChild(cellDate);
+            row.appendChild(cellTime);
+            row.appendChild(cellSprint);
+            row.appendChild(cellCircuit);
+
+            tableBody.appendChild(row);
+        });
+    
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+fillTableRacesSchedule();
+
+const modal = document.querySelector('.modal');
+const modalForm = document.querySelector('.modal-form')
+const spanClose = document.querySelector('.close');
+const btnConfirm = document.querySelector('#btn-confirm');
+const btnCancel = document.querySelector('#btn-cancel');
+
+const loadMenuClick = (idButton) => {
+    if (idButton === 'mn-rc-schd') {
+        modalForm.innerHTML = '';
+        
+        const label = document.createElement('label');
+        label.innerText = 'Year:';
+        const input = document.createElement('input');
+        input.id = 'inp-md-year'
+        input.type = 'number';
+
+        const div = document.createElement('div');
+        div.appendChild(label)
+        div.appendChild(input);
+        
+        modalForm.appendChild(div);
+
+        modal.style.display = 'block';
+    } else if (idButton === 'mn-drv-stdngs') {
+        
+    } else if (idButton === 'mn-cntrtr-stdngs') {
+        
+    } else if (idButton === 'mn-rc-anls') {
+        
+    } else if (idButton === 'mn-qly-anls') {
+        
+    }
+}
+
+menuButtons.forEach(menuButton => {
+    menuButton.addEventListener('click', () => {
+        clickedButtonId = menuButton.id;
+        loadMenuClick(menuButton.id);
+    });
+});
+
+/* Modal */
+
+
+
+spanClose.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+btnConfirm.addEventListener('click', () => {
+    modal.style.display = 'none';
+
+    const inputModalYear = document.querySelector('#inp-md-year');
+
+    if (clickedButtonId === 'mn-rc-schd') {
+        fillTableRacesSchedule(inputModalYear.value);
+    } else if (clickedButtonId === 'mn-drv-stdngs') {
+        
+    } else if (clickedButtonId === 'mn-cntrtr-stdngs') {
+        
+    } else if (clickedButtonId === 'mn-rc-anls') {
+        
+    } else if (clickedButtonId === 'mn-qly-anls') {
+        
+    }
+});
+
+btnCancel.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
--- services/services.js
+++ services/services.js
@@ -1,6 +1,8 @@
+var protocolType = 'https';
+
 export const fetchDrivers = async (year, round) => {
     try {
-        const response = await fetch(`http://ergast.com/api/f1/${year}/${round}/drivers.json`); //?limit=2
+        const response = await fetch(`${protocolType}://ergast.com/api/f1/${year}/${round}/drivers.json`); //?limit=2
     if (response.ok) {
         const drivers = await response.json();
         if (drivers) {
@@ -20,13 +22,21 @@ export const fetchDrivers = async (year, round) => {
         };
     }
     } catch (error) {
+        if (protocolType === 'https') {
+            protocolType = 'http';
+            try {
+                fetchDrivers(year, round);
+            } catch (error) {
+                return {error};
+            }
+        }
         return {error};
     }
 }
 
 export const fetchDriverConstructor = async (year, driver) => {
     try {
-        const response = await fetch(`http://ergast.com/api/f1/${year}/drivers/${driver}/constructors.json`);
+        const response = await fetch(`${protocolType}://ergast.com/api/f1/${year}/drivers/${driver}/constructors.json`);
     if (response.ok) {
         const driverConstructor = await response.json();
         if (driverConstructor) {
@@ -46,58 +56,116 @@ export const fetchDriverConstructor = async (year, driver) => {
         };
     }
     } catch (error) {
+        if (protocolType === 'https') {
+            protocolType = 'http';
+            try {
+                fetchDriverConstructor(year, driver);
+            } catch (error) {
+                return {error};
+            }
+        }
         return {error};
     }
 }
 
 export const fetchLapTime = async (year, round, driver) => {
     try {
-        const response = await fetch(`http://ergast.com/api/f1/${year}/${round}/drivers/${driver}/laps.json?limit=100`);
-    if (response.ok) {
-        const laptimes = await response.json();
-        if (laptimes) {
-            return laptimes['MRData'];
+        const response = await fetch(`${protocolType}://ergast.com/api/f1/${year}/${round}/drivers/${driver}/laps.json?limit=100`);
+        if (response.ok) {
+            const laptimes = await response.json();
+            if (laptimes) {
+                return laptimes['MRData'];
+            } else {
+                return {
+                    error: 'Erro ao converter dados retornados.',
+                    status: response.status,
+                    message: response.statusText
+                };
+            }
         } else {
             return {
-                error: 'Erro ao converter dados retornados.',
+                error: 'Erro na requisição',
                 status: response.status,
                 message: response.statusText
             };
         }
-    } else {
-        return {
-            error: 'Erro na requisição',
-            status: response.status,
-            message: response.statusText
-        };
-    }
     } catch (error) {
+        if (protocolType === 'https') {
+            protocolType = 'http';
+            try {
+                fetchLapTime(year, round, driver);
+            } catch (error) {
+                return {error};
+            }
+        }
         return {error};
     }
 }
 
 export const fetchRaceSchedule = async (year = 'current') => {
     try {
-        const response = await fetch(`http://ergast.com/api/f1/${year}.json`);
-    if (response.ok) {
-        const races = await response.json();
-        if (races) {
-            return races.MRData.RaceTable.Races;
+        const response = await fetch(`${protocolType}://ergast.com/api/f1/${year}.json`);
+        if (response.ok) {
+            const races = await response.json();
+            if (races) {
+                return races.MRData.RaceTable.Races;
+            } else {
+                return {
+                    error: 'Erro ao converter dados retornados.',
+                    status: response.status,
+                    message: response.statusText
+                };
+            }
         } else {
             return {
-                error: 'Erro ao converter dados retornados.',
+                error: 'Erro na requisição',
                 status: response.status,
                 message: response.statusText
             };
         }
-    } else {
-        return {
-            error: 'Erro na requisição',
-            status: response.status,
-            message: response.statusText
-        };
-    }
     } catch (error) {
+        if (protocolType === 'https') {
+            protocolType = 'http';
+            try {
+                fetchRaceSchedule(year);
+            } catch (error) {
+                return {error};
+            }
+        }
+        return {error};
+    }
+}
+
+export const fetchDriversStandings = async (year) => {
+    try {
+        const response = await fetch(`${protocolType}://ergast.com/api/f1/${year}/driverStandings.json`);
+        if (response.ok) {
+            const drivers = await response.json();
+            if (drivers) {
+                return drivers.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+            } else {
+                return {
+                    error: 'Erro ao converter dados retornados.',
+                    status: response.status,
+                    message: response.statusText
+                };
+            }
+        } else {
+            return {
+                error: 'Erro na requisição',
+                status: response.status,
+                message: response.statusText
+            };
+        }
+    } catch (error) {
+        if (protocolType === 'https') {
+            protocolType = 'http';
+            try {
+                fetchDriversStandings(year);
+            } catch (error) {
+                return {error};
+            }
+        }
         return {error};
     }
 }
